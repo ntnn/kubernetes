dc4234ce1eeb11358b2c89272e5567742e3b7efd
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-09-27 16:03:58 +0200

    UPSTREAM: <carry>: apiserver: partial wildcard metadata request accross identities
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- staging/src/k8s.io/apiserver/pkg/storage/etcd3/store.go
+++ staging/src/k8s.io/apiserver/pkg/storage/etcd3/store.go
@@ -30,6 +30,7 @@ import (
 	clientv3 "go.etcd.io/etcd/client/v3"
 	"go.etcd.io/etcd/client/v3/kubernetes"
 	"go.opentelemetry.io/otel/attribute"
+	"k8s.io/apiserver/pkg/kcp"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
@@ -755,6 +756,7 @@ func (s *store) GetList(ctx context.Context, key string, opts storage.ListOption
 	if err != nil {
 		return storage.NewInternalError(fmt.Errorf("unable to get cluster for list key %q: %v", keyPrefix, err))
 	}
+	crdIndicator := kcp.CustomResourceIndicatorFrom(ctx)
 
 	// loop until we have filled the requested limit from etcd or there are no more results
 	var lastKey []byte
