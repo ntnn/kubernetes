cad7ebcedfae205b9e55d34dc47cdaf5200d8d7c
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2023-09-27 13:49:07 +0200

    UPSTREAM: <carry>: storage/etcd3: clusterize
    Co-authored-by: Marvin Beckers <marvin@kubermatic.com>
    Signed-off-by: Marvin Beckers <marvin@kubermatic.com>
    Signed-off-by: Dr. Stefan Schimanski <stefan.schimanski@gmail.com>

--- staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
+++ staging/src/k8s.io/apiserver/pkg/storage/etcd3/store_test.go
@@ -33,6 +33,9 @@ import (
 	"go.etcd.io/etcd/client/v3/kubernetes"
 	"go.etcd.io/etcd/server/v3/embed"
 	"google.golang.org/grpc/grpclog"
+	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
+
+	"github.com/kcp-dev/logicalcluster/v3"
 
 	"k8s.io/apimachinery/pkg/api/apitesting"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -469,6 +472,7 @@ func TestLeaseMaxObjectCount(t *testing.T) {
 		ReuseDurationSeconds: defaultLeaseReuseDurationSeconds,
 		MaxObjectCount:       2,
 	}))
+	ctx = genericapirequest.WithCluster(ctx, genericapirequest.Cluster{Name: logicalcluster.Name("root")})
 
 	obj := &example.Pod{ObjectMeta: metav1.ObjectMeta{Name: "foo"}}
 	out := &example.Pod{}
