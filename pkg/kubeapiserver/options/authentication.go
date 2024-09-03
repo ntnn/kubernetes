0903019f72378e9b771e92d674105267d72c87ad
Mangirdas Judeikis <mangirdas@judeikis.lt>
2024-09-03 15:32:27 +0300

    UPSTREAM: <carry>: generic cleanup

--- pkg/kubeapiserver/options/authentication.go
+++ pkg/kubeapiserver/options/authentication.go
@@ -49,7 +49,6 @@ import (
 	authenticationconfigmetrics "k8s.io/apiserver/pkg/server/options/authenticationconfig/metrics"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	"k8s.io/apiserver/plugin/pkg/authenticator/token/oidc"
-	"k8s.io/client-go/informers"
 	"k8s.io/client-go/util/keyutil"
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/klog/v2"
@@ -136,7 +135,7 @@ type ServiceAccountAuthenticationOptions struct {
 	IsTokenSignerExternal bool
 	// OptionalTokenGetter is a function that returns a service account token getter.
 	// If not set, the default token getter will be used.
-	OptionalTokenGetter func(factory informers.SharedInformerFactory) serviceaccount.ServiceAccountTokenGetter
+	OptionalTokenGetter func(factory kcpinformers.SharedInformerFactory) serviceaccount.ServiceAccountTokenClusterGetter
 	// ExternalPublicKeysGetter gets set if `--service-account-signing-endpoint` is passed.
 	// ExternalPublicKeysGetter is mutually exclusive with KeyFiles.
 	ExternalPublicKeysGetter serviceaccount.PublicKeysGetter
