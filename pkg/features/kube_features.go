a114d4bd5e34af5f2c473fa121cd607c51cf7ab0
Karol Szwaj <karol.szwaj@gmail.com>
2025-03-13 11:08:06 +0100

    UPSTREAM: <carry>: Add kube feature gate for global service account
    TODO: this commit may be dropped once https://github.com/kcp-dev/kcp/pull/3274 and related issues are done
    
    Signed-off-by: Karol Szwaj <karol.szwaj@gmail.com>
    
    On-behalf-of: @SAP karol.szwaj@sap.com
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -2155,6 +2155,9 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 	zpagesfeatures.ComponentStatusz: {
 		{Version: version.MustParse("1.32"), Default: false, PreRelease: featuregate.Alpha},
 	},
+	GlobalServiceAccount: {
+		{Version: version.MustParse("1.31"), Default: false, PreRelease: featuregate.Alpha},
+	},
 }
 
 // defaultKubernetesFeatureGateDependencies enumerates the dependencies of any feature gate that
