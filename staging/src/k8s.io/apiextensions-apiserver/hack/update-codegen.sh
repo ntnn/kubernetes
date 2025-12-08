01f81c49ab5d452a53b0baae9fcbc43793129d4d
Mangirdas Judeikis <mangirdas@judeikis.lt>
2025-12-08 11:23:02 +0200

    CARRY: Disable protoc support

--- staging/src/k8s.io/apiextensions-apiserver/hack/update-codegen.sh
+++ staging/src/k8s.io/apiextensions-apiserver/hack/update-codegen.sh
@@ -53,5 +53,4 @@ kube::codegen::gen_client \
     --output-pkg "${THIS_PKG}/pkg/client" \
     --versioned-name "clientset" \
     --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
-    --prefers-protobuf \
     "${SCRIPT_ROOT}/pkg/apis"
--- staging/src/k8s.io/metrics/hack/update-codegen.sh
+++ staging/src/k8s.io/metrics/hack/update-codegen.sh
@@ -36,5 +36,4 @@ kube::codegen::gen_client \
     --output-dir "${SCRIPT_ROOT}/pkg/client" \
     --output-pkg "${THIS_PKG}/pkg/client" \
     --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
-    --prefers-protobuf \
     "${SCRIPT_ROOT}/pkg/apis"
