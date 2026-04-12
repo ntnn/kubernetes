08d085967e6fddf73bd3b7e856769613d5d80bf4
Nelo-T. Wallus <n.wallus@sap.com>
2026-04-12 15:52:10 +0200

    UPSTREAM: <carry>: Emit events for the correct cluster
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- pkg/controller/garbagecollector/kcp.go
+++ pkg/controller/garbagecollector/kcp.go
@@ -19,7 +19,12 @@ package garbagecollector
 import (
 	"context"
 
+	kcpcache "github.com/kcp-dev/apimachinery/v2/pkg/cache"
+	kcpkubernetesclientset "github.com/kcp-dev/client-go/kubernetes"
 	"github.com/kcp-dev/logicalcluster/v3"
+	v1 "k8s.io/api/core/v1"
+	v1core "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/record"
 )
 
 type clusterContextKey struct{}
@@ -35,4 +40,52 @@ func ClusterFrom(ctx context.Context) logicalcluster.Name {
 
 func contextForCluster(ref objectReference) context.Context {
 	return WithCluster(context.TODO(), ref.Cluster)
+}
+
+var _ record.EventSink = (*clusterEventSinkImpl)(nil)
+
+// clusterEventSinkImpl allows the non-cluster aware garbage collector
+// to emit cluster aware events.
+//
+// The core problem is that the broadcaster writes to sinks and events
+// are written to recorders build from broadcasters:
+// eventf(ref) -> recorder -> broadcaster -> sink
+//
+// To make this chain cluster aware either the object reference would
+// have to be cluster aware - which could produce side effects because
+// no other code is expected object references to be cluster-aware. It
+// could also confuse other tooling if starting to rely on this being
+// cluster aware.
+//
+// Instead the producer is patched to put the cluster name into the name
+// on the object reference and this sink then extracts the name from
+// there.
+// This works because the recorder and broadcaster are accepting the
+// object reference as-is and place it in the InvolvedObject property of
+// the event to be emitted, allowing the sink to extract the cluster
+// name and fix the name.
+type clusterEventSinkImpl struct {
+	client *kcpkubernetesclientset.ClusterClientset
+}
+
+func (sink *clusterEventSinkImpl) getClient(event *v1.Event) v1core.EventInterface {
+	cluster, _, name, err := kcpcache.SplitMetaClusterNamespaceKey(event.InvolvedObject.Name)
+	if err != nil {
+		panic("error splitting name into cluster and name in garbage collector")
+	}
+
+	event.InvolvedObject.Name = name
+	return sink.client.Cluster(cluster.Path()).CoreV1().Events("")
+}
+
+func (sink *clusterEventSinkImpl) Create(event *v1.Event) (*v1.Event, error) {
+	return sink.getClient(event).CreateWithEventNamespace(event)
+}
+
+func (sink *clusterEventSinkImpl) Update(event *v1.Event) (*v1.Event, error) {
+	return sink.getClient(event).UpdateWithEventNamespace(event)
+}
+
+func (sink *clusterEventSinkImpl) Patch(event *v1.Event, data []byte) (*v1.Event, error) {
+	return sink.getClient(event).PatchWithEventNamespace(event, data)
 }
